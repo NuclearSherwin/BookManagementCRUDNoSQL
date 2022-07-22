b9d7bc94055dcf78f9b6a9845da59d359ff59f4c
paika2060 <paika2060@gmail.com>
2022-07-22 21:34:01 +0700

    upload project but not done yet, it still have to develop some small feature

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,76 @@
+﻿using BookManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+namespace BookMan.Mvc.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly Service _service;
+        public BookController(Service service)
+        {
+            _service = service;
+        }
+        public IActionResult Index()
+        {
+            return View(_service.Get());
+        }
+
+        public IActionResult Details(int id)
+        {
+            var book = _service.Get(id);
+            if (book == null) return NotFound();
+            return View(book);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var b = _service.Get(id);
+            if (b == null) return NotFound();
+            return View(b);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Book book)
+        {
+            _service.Delete(book.Id);
+            _service.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            var bookUpdate = _service.Get(id);
+            if (bookUpdate == null) return NotFound();
+            return View(bookUpdate);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                _service.Update(book);
+                _service.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(book);
+
+        }
+
+        public IActionResult Create() => base.View(_service.Create());
+
+        [HttpPost]
+        public IActionResult Create(Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                _service.Add(book);
+                _service.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(book);
+        }
+    }
+}
---
+++ Models/Book.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookManagement.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, DisplayName("Title")]
+        public string Name { get; set; }
+
+        [Required]
+        public string Authors { get; set; }
+
+        [Required]
+        public string Publisher { get; set; }
+
+        [Required, Range(1990, int.MaxValue), DisplayName("Date public")]
+        public int Year { get; set; }
+
+        [DisplayName("Summary")]
+        public string Description { get; set; }
+
+        [DisplayName("File")]
+        public string DateFile { get; set; }
+
+    }
+}
